a3beb678ca730d4f00370c960e1835bc155579a7
Matheus <math.assuncao@gmail.com>
2020-10-06 18:52:25 -0300

    Commit including script and testing gitignore

--- course_project_1.R
+++ course_project_1.R
@@ -1,8 +1,22 @@
-library(ggplot2)
+# Exploratory Data Analysis
 
-x <- rnorm(100)
-y <- rnorm(100)
+  # Course Project 1
+    library(dplyr)
 
-plot(x,y)
-
-plot(y,z)
+  # Preparing the data to work
+    url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+    file1 <- download.file(url, destfile = "household_power_consumption.zip")
+    file2 <- unzip("household_power_consumption.zip")
+    classes <- c("character", "character", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric")
+    data <- read.table(file2, header = TRUE, sep = ";", na.strings = "?", colClasses = classes)
+    
+  # Correcting dates  
+    data <- mutate(data, Date = as.Date(Date, format='%d/%m/%Y'))
+    data <- mutate(data, Time = as.Date(Time, format='%H:%M:%S'))
+    
+  # Subsetting for the relevant dates
+    date1 <- as.Date("2007-02-01 ")
+    date2 <- as.Date("2007-02-02")
+    data <- data[data$Date >= date1 & data$Date <= date2,]
+    
+  # Making the Graphics
